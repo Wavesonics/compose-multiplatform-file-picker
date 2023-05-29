275d94428513264707e0c706da15af88801806ff
shalva97 <shalva258@gmail.com>
2023-05-29 23:53:31 +0400

    kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
+// TODO remove this when Gradle is updated 8.1 https://github.com/gradle/gradle/issues/22797
 @file:Suppress("DSL_SCOPE_VIOLATION")
 
-// TODO remove this when Gradle is updated 8.1 https://github.com/gradle/gradle/issues/22797
 
 allprojects {
     repositories {
@@ -11,8 +11,8 @@ allprojects {
 }
 
 plugins {
-    kotlin("multiplatform") apply false
-    kotlin("android") apply false
+    kotlin("multiplatform") version libs.versions.kotlin apply false
+    kotlin("android") version libs.versions.kotlin apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,8 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("multiplatform").version(extra["kotlin.version"] as String)
-        kotlin("android").version(extra["kotlin.version"] as String)
-        id("com.android.application").version(extra["agp.version"] as String)
-        id("com.android.library").version(extra["agp.version"] as String)
-//        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+//        kotlin("multiplatform").version(extra["kotlin.version"] as String)
+//        kotlin("android").version(extra["kotlin.version"] as String)
     }
 }
 
