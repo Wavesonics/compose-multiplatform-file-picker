5d1a88dc9b84b949c00365cd6c2ea2f9bb0d01e3
shalva97 <shalva258@gmail.com>
2023-07-01 21:55:54 +0400

    cleanup

--- mpfilepicker/src/jvmMain/kotlin/com/darkrockstudios/libraries/mpfilepicker/DesktopFilePicker.kt
+++ mpfilepicker/src/jvmMain/kotlin/com/darkrockstudios/libraries/mpfilepicker/DesktopFilePicker.kt
@@ -9,60 +9,56 @@ import kotlinx.coroutines.withContext
 import java.io.File
 
 public data class JvmFile(
-    override val path: String,
-    override val platformFile: File,
+	override val path: String,
+	override val platformFile: File,
 ) : MPFile<File>
 
 @Composable
 public actual fun FilePicker(
-    show: Boolean,
-    initialDirectory: String?,
-    fileExtensions: List<String>,
-    onFileSelected: FileSelected
+	show: Boolean,
+	initialDirectory: String?,
+	fileExtensions: List<String>,
+	onFileSelected: FileSelected
 ) {
-    val scope = rememberCoroutineScope()
-    LaunchedEffect(show) {
-        if (show) {
-            scope.launch(Dispatchers.Default) {
-                val fileFilter = if (fileExtensions.isNotEmpty()) {
-                    fileExtensions.joinToString(",")
-                } else {
-                    ""
-                }
+	LaunchedEffect(show) {
+		if (show) {
+			val fileFilter = if (fileExtensions.isNotEmpty()) {
+				fileExtensions.joinToString(",")
+			} else {
+				""
+			}
 
-                val initialDir = initialDirectory ?: System.getProperty("user.dir")
-                val filePath = FileChooser.chooseFile(
-                    initialDirectory = initialDir,
-                    fileExtensions = fileFilter
-                )
-                withContext(Dispatchers.Main) {
-                    if(filePath != null) {
-                        onFileSelected(JvmFile(filePath, File(filePath)))
-                    } else {
-                        onFileSelected(null)
-                    }
-                }
-            }
-        }
-    }
+			val initialDir = initialDirectory ?: System.getProperty("user.dir")
+			val filePath = FileChooser.chooseFile(
+				initialDirectory = initialDir,
+				fileExtensions = fileFilter
+			)
+			if (filePath != null) {
+				onFileSelected(JvmFile(filePath, File(filePath)))
+			} else {
+				onFileSelected(null)
+			}
+
+		}
+	}
 }
 
 @Composable
 public actual fun DirectoryPicker(
-    show: Boolean,
-    initialDirectory: String?,
-    onFileSelected: (String?) -> Unit
+	show: Boolean,
+	initialDirectory: String?,
+	onFileSelected: (String?) -> Unit
 ) {
-    val scope = rememberCoroutineScope()
-    LaunchedEffect(show) {
-        if (show) {
-            scope.launch(Dispatchers.Default) {
-                val initialDir = initialDirectory ?: System.getProperty("user.dir")
-                val fileChosen = FileChooser.chooseDirectory(initialDir)
-                withContext(Dispatchers.Main) {
-                    onFileSelected(fileChosen)
-                }
-            }
-        }
-    }
+	val scope = rememberCoroutineScope()
+	LaunchedEffect(show) {
+		if (show) {
+			scope.launch(Dispatchers.Default) {
+				val initialDir = initialDirectory ?: System.getProperty("user.dir")
+				val fileChosen = FileChooser.chooseDirectory(initialDir)
+				withContext(Dispatchers.Main) {
+					onFileSelected(fileChosen)
+				}
+			}
+		}
+	}
 }
