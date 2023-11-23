2f7131feb38b81a7fc71737246f37e764e60fa00
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-11-23 13:31:49 +0100

    impl for macOs

--- mpfilepicker/src/macosX64Main/kotlin/com/darkrockstudios/libraries/mpfilepicker/DesktopFilePicker.kt
+++ mpfilepicker/src/macosX64Main/kotlin/com/darkrockstudios/libraries/mpfilepicker/DesktopFilePicker.kt
@@ -2,18 +2,34 @@ package com.darkrockstudios.libraries.mpfilepicker
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
 import platform.AppKit.NSOpenPanel
 import platform.AppKit.setAllowedFileTypes
+import platform.Foundation.NSData
 import platform.Foundation.NSURL
+import platform.posix.memcpy
 
 public data class MacOSFile(
 	override val path: String,
 	override val platformFile: NSURL,
-) : MPFile<NSURL>
+) : MPFile<NSURL> {
+	@OptIn(ExperimentalForeignApi::class)
+	public fun NSData.toByteArray(): ByteArray = ByteArray(this@toByteArray.length.toInt()).apply {
+		usePinned {
+			memcpy(it.addressOf(0), this@toByteArray.bytes, this@toByteArray.length)
+		}
+	}
+	override suspend fun getFileByteArray(): ByteArray = platformFile.dataRepresentation.toByteArray()
+}
 
 @Composable
 public actual fun FilePicker(
-	show: Boolean, initialDirectory: String?, fileExtensions: List<String>, onFileSelected: FileSelected
+	show: Boolean,
+	initialDirectory: String?,
+	fileExtensions: List<String>,
+	onFileSelected: FileSelected,
 ) {
 	LaunchedEffect(show) {
 		if (show) {
@@ -36,8 +52,45 @@ public actual fun FilePicker(
 }
 
 @Composable
+public actual fun MultipleFilePicker(
+	show: Boolean,
+	initialDirectory: String?,
+	fileExtensions: List<String>,
+	onFileSelected: FilesSelected,
+) {
+	LaunchedEffect(show) {
+		if (show) {
+			with(NSOpenPanel()) {
+				if (initialDirectory != null) directoryURL = NSURL.fileURLWithPath(initialDirectory, true)
+				allowsMultipleSelection = true
+				setAllowedFileTypes(fileExtensions)
+				allowsOtherFileTypes = true
+				canChooseDirectories = false
+				canChooseFiles = true
+				runModal()
+
+				val filesUrls = URLs
+
+				val files: List<MacOSFile> = filesUrls.mapNotNull { file ->
+					(file as? NSURL)?.let { nsUrl ->
+						nsUrl.path?.let { path ->
+							MacOSFile(path, nsUrl)
+						}
+					}
+				}
+
+				if (files.isEmpty()) onFileSelected(null)
+				else onFileSelected(files)
+			}
+		}
+	}
+}
+
+@Composable
 public actual fun DirectoryPicker(
-	show: Boolean, initialDirectory: String?, onFileSelected: (String?) -> Unit
+	show: Boolean,
+	initialDirectory: String?,
+	onFileSelected: (String?) -> Unit
 ) {
 	LaunchedEffect(show) {
 		if (show) {
