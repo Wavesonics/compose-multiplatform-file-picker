0c0b9da7bc44431f6d14ab725ec6abd79e8a3b9f
Shubham Singh <shubhamsinghshubham777@gmail.com>
2023-12-26 09:56:45 +0530

    Fix library crash while getting byteArray for CONTENT AndroidFile scheme

--- mpfilepicker/src/androidMain/kotlin/com/darkrockstudios/libraries/mpfilepicker/AndroidFilePicker.kt
+++ mpfilepicker/src/androidMain/kotlin/com/darkrockstudios/libraries/mpfilepicker/AndroidFilePicker.kt
@@ -1,18 +1,26 @@
 package com.darkrockstudios.libraries.mpfilepicker
 
+import android.content.Context
 import android.net.Uri
 import android.webkit.MimeTypeMap
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.net.toFile
 
 public data class AndroidFile(
 	override val path: String,
 	override val platformFile: Uri,
+	private val context: Context,
 ) : MPFile<Uri> {
-	override suspend fun getFileByteArray(): ByteArray = platformFile.toFile().readBytes()
+	override suspend fun getFileByteArray(): ByteArray {
+		return context.contentResolver
+			.openInputStream(platformFile)
+			.use { inputStream -> inputStream?.readBytes() }
+			?: platformFile.toFile().readBytes()
+	}
 }
 
 @Composable
@@ -23,9 +31,10 @@ public actual fun FilePicker(
 	title: String?,
 	onFileSelected: FileSelected
 ) {
+	val context = LocalContext.current
 	val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.OpenDocument()) { result ->
 		if (result != null) {
-			onFileSelected(AndroidFile(result.toString(), result))
+			onFileSelected(AndroidFile(result.toString(), result, context))
 		} else {
 			onFileSelected(null)
 		}
@@ -54,13 +63,14 @@ public actual fun MultipleFilePicker(
 	title: String?,
 	onFileSelected: FilesSelected
 ) {
+	val context = LocalContext.current
 	val launcher = rememberLauncherForActivityResult(
 		contract = ActivityResultContracts.OpenMultipleDocuments()
 	) { result ->
 
 		val files = result.mapNotNull { uri ->
 			uri.path?.let {path ->
-				AndroidFile(path, uri)
+				AndroidFile(path, uri, context)
 			}
 		}
 
