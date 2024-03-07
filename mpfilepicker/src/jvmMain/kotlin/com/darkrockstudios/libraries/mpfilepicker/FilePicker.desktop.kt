f5701a60228eaf4dfc3e129d44e51bcb8800d872
Vincent Guillebaud <v.guillebaud@akawan.fr>
2024-03-07 11:36:43 +0100

    🚧 wip

--- mpfilepicker/src/jvmMain/kotlin/com/darkrockstudios/libraries/mpfilepicker/FilePicker.desktop.kt
+++ mpfilepicker/src/jvmMain/kotlin/com/darkrockstudios/libraries/mpfilepicker/FilePicker.desktop.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import java.io.File
 
+
 public actual data class PlatformFile(
 	val file: File,
 )
@@ -24,19 +25,68 @@ public actual fun FilePicker(
 				""
 			}
 
-			val initialDir = initialDirectory ?: System.getProperty("user.dir")
-			val filePath = chooseFile(
-				initialDirectory = initialDir,
-				fileExtension = fileFilter,
-				title = title
-			)
-			if (filePath != null) {
-				val file = File(filePath)
-				val platformFile = PlatformFile(file)
-				onFileSelected(platformFile)
-			} else {
-				onFileSelected(null)
-			}
+
+//			val pool = Foundation.NSAutoreleasePool()
+//			try {
+//				Foundation.executeOnMainThread(
+//					withAutoreleasePool = false,
+//					waitUntilDone = true,
+//				) {
+//					val openPanel = Foundation.invoke("NSOpenPanel", "new")
+//					Foundation.invoke(openPanel, "runModal")
+//					val url = Foundation.invoke(openPanel, "URL")
+//					val path = Foundation.invoke(url, "path")
+//					val filePath = Foundation.toStringViaUTF8(path)
+//					if (filePath != null) {
+//						val file = File(filePath)
+//						onFileSelected(PlatformFile(file))
+//					} else {
+//						onFileSelected(null)
+//					}
+//				}
+//			} finally {
+//				pool.drain()
+//			}
+
+//			val foundationLibrary = Native.load("Foundation", FoundationLibrary::class.java)
+//			val handler = Proxy.getInvocationHandler(foundationLibrary) as Library.Handler
+//			val nativeLibrary = handler.nativeLibrary
+//			val objcMsgSend = nativeLibrary.getFunction("objc_msgSend")
+//
+//			val className = "NSOpenPanel"
+//			val objecClass = foundationLibrary.objc_getClass(className)
+//
+//			val selectorStr = "alloc"
+//			val selector = foundationLibrary.sel_registerName(selectorStr)
+//
+//			val args = arrayOf(
+//				objecClass,
+//				selector,
+//			)
+//
+//			val res = objcMsgSend.invokeLong(args)
+//			val resPointer = Pointer(res)
+//			val nsOpenPanel = NSOpenPanel::class.java.cast(Proxy.newProxyInstance(
+//				NSOpenPanel::class.java.classLoader,
+//				arrayOf(NSOpenPanel::class.java),
+//				))
+//
+//			print("res: $nsOpenPanel")
+
+
+//			val initialDir = initialDirectory ?: System.getProperty("user.dir")
+//			val filePath = chooseFile(
+//				initialDirectory = initialDir,
+//				fileExtension = fileFilter,
+//				title = title
+//			)
+//			if (filePath != null) {
+//				val file = File(filePath)
+//				val platformFile = PlatformFile(file)
+//				onFileSelected(platformFile)
+//			} else {
+//				onFileSelected(null)
+//			}
 
 		}
 	}
@@ -89,3 +139,21 @@ public actual fun DirectoryPicker(
 		}
 	}
 }
+
+//interface NSOpenPanel : Library {
+//	companion object {
+//		val INSTANCE: NSOpenPanel = Native.load("NSOpenPanel", NSOpenPanel::class.java)
+//	}
+//}
+//
+//interface FoundationLibrary : Library {
+//	fun objc_getClass(className: String?): Pointer
+//
+//	fun sel_registerName(selectorName: String): Pointer
+//}
+//
+//class ObjcToJava : InvocationHandler {
+//	override fun invoke(proxy: Any, method: Method, args: Array<Any>): Any {
+//		TODO("Not yet implemented")
+//	}
+//}
